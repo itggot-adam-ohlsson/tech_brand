337846ad4d8281c5589b4cd7f458b122ddebb0e3
itggot-adam-ohlsson <adam.olsson@itggot.se>
2017-04-03 21:11:21 +0200

    Prototyp av sökfunktionen

---
+++ site/js/search.js
@@ -0,0 +1,34 @@
+var searches = [];
+var elements = document.querySelectorAll(".search");
+
+ for(var i = 0; i < elements.length; i++) {
+  var current = elements[i];
+  searches.push(current.textContent.toLowerCase());
+ }
+
+function recent() {
+  var input = document.querySelector("input").value.toLowerCase();
+  document.querySelector(".search_result").innerHTML = "";
+  for (i = 0; i < searches.length; i++) {
+    if (searches[i].includes(input) && input.length > 2) {
+      var tiles = document.createElement("div");
+      document.querySelector(".search_result").appendChild(tiles);
+      tiles.className = "search_tiles";
+      var print = document.createTextNode(searches[i]);
+      tiles.appendChild(print);
+    }
+  }
+}
+setInterval(recent, 200);
+
+function search() {
+  var input = document.querySelector("input").value;
+  console.log(input);
+
+  if (searches.includes(input)) {
+    window.location.assign(input + ".html");
+  }
+  else {
+
+  }
+}
