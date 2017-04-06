f01ff9758d5d275b9f8eda1d3a8df4b104533c6d
itggot-adam-ohlsson <adam.olsson@itggot.se>
2017-04-06 21:48:28 +0200

    Scenario1 Klart

--- site/js/search.js
+++ site/js/search.js
@@ -1,4 +1,4 @@
-var searches = [];
+var searches = ["logitech fury"];
 var elements = document.querySelectorAll(".search");
 
  for(var i = 0; i < elements.length; i++) {
@@ -6,11 +6,13 @@ var elements = document.querySelectorAll(".search");
   searches.push(current.textContent.toLowerCase());
  }
 
+/* Funktion för att visa sökresultat när minst 2 tecken skrivits in i
+sökfälet. */
 function recent() {
   var input = document.querySelector("input").value.toLowerCase();
   document.querySelector(".search_result").innerHTML = "";
   for (i = 0; i < searches.length; i++) {
-    if (searches[i].includes(input) && input.length > 2) {
+    if (searches[i].includes(input) && input.length > 1) {
       var tiles = document.createElement("div");
       document.querySelector(".search_result").appendChild(tiles);
       tiles.className = "search_tiles";
@@ -21,6 +23,8 @@ function recent() {
 }
 setInterval(recent, 200);
 
+/* Funktion för att komma till sökresultat när man har sökt
+på en produkt. */
 function search() {
   var input = document.querySelector("input").value;
   console.log(input);
@@ -28,7 +32,14 @@ function search() {
   if (searches.includes(input)) {
     window.location.assign(input + ".html");
   }
-  else {
-
-  }
 }
+
+/* Funktion för att komma till vald sida efter att man klickat på en
+produktkategori i navigeringsfältet. */
+$("h3").click(function() {
+  window.location.assign($(this).html() + "Kat.html");
+});
+
+$(".button").click(function () {
+  window.location.assign($(this).find(".product_id").html() + "Kund.html");
+});
