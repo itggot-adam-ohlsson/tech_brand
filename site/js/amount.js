5e197d92e666ce9c8b8a2f6ea758b213a59bd4e7
Adam Ohlsson <adam.olsson@itggot.se>
2017-04-07 15:01:16 +0200

    Börjat på checkout!

---
+++ site/js/amount.js
@@ -0,0 +1,18 @@
+var amount = document.querySelector("#amount_number");
+var price = document.querySelector(".product_price");
+n = 1;
+var startPrice = parseInt(document.querySelector(".product_price").innerHTML);
+
+  $("#plus").click( function functionName() {
+    n = n + 1;
+    amount.innerHTML = n;
+    price.innerHTML = parseInt(price.innerHTML) + startPrice + ":-";
+  });
+  $("#minus").click( function functionName() {
+    if (n == 1) {
+      window.location.assign("kundvagn.html");
+    }
+    n = n - 1;
+    amount.innerHTML = n;
+    price.innerHTML = parseInt(price.innerHTML) - startPrice + ":-";
+  });
